11362fff196810d2badf3204c805769404884129
David <davidtruhlarac@gmail.com>
2024-03-29 16:34:11 +0100

    test_product

--- ssr/eshop/resources/views/product_detail.blade.php
+++ ssr/eshop/resources/views/product_detail.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.app')
 
+ <!-- Len pre testovacie účely -->
+@php
+$product = (object) [
+    'name' => 'Name',
+    'slug' => 'product-slug-123',
+    'thumbnail' => 'https://picsum.photos/1024',
+    'status' => 'Na sklade',
+    'manufacturer' => 'Oral-B',
+    'manufacturerSlug' => 'vyrobca-123',
+    'price' => '99.99',
+    'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'shortDescription' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    'category' => 'Category XYZ',
+    'gallery' => [
+        'https://picsum.photos/200',
+        'https://picsum.photos/200',
+        'https://picsum.photos/200',
+    ],
+];
+@endphp
+
 @section('title', config('urls.product_detail.getPathBuilder')('QWER'))
 
 @section('content')
@@ -16,25 +37,23 @@
     </script>
     <div class="container pt-5">
             <div class="detail d-flex gap-5 flex-column flex-md-row">
-                <div class="d-flex flex-column p-0 w-100">
-                    <img src="https://picsum.photos/1024" alt="" />
+                <div class="d-flex flex-column p-0 col-md-7">
+                    <img src=" {{ $product->thumbnail }} " alt="{{ $product->name }}" />
                 </div>
-                <div class="d-flex flex-column gap-5 p-0">
+                <div class="d-flex flex-column gap-5 p-2 col-md-5">
                     <div class="d-flex flex-column h-100 justify-content-center gap-6">
                         <div class="d-flex flex-column">
                             <img src="https://picsum.photos/400" alt="výrobca" class="max-w-[100px] h-[70px] object-cover">
-                            <h2 class="text-4xl mb-2 font-semibold">Názov produktu</h2>
-                            <p class="">Na sklade</p>
+                            <h2 class="text-4xl mb-2 font-semibold">{{ $product->name }}</h2>
+                            <p class="">{{ $product->status }}</p>
                         </div>
                         
                         <div class="d-flex flex-column">
                             <div class="d-flex flex-column">
-                                <h4 class="text-secondary text-2xl mb-2">99.99€</h4>
+                                <h4 class="text-secondary text-2xl mb-2">{{ $product->price }}</h4>
                             </div>
                             <p>
-                                Renew your style with the latest designer trends
-                                in men's clothing or achieve a perfectly curated
-                                wardrobe thanks to our line of timeless pieces.
+                                {{ $product->shortDescription }}
                             </p>
                         </div>
                         <div class="d-flex flex-row align-items-between">
@@ -60,37 +79,17 @@
                     <div class="d-flex flex-column">
                         <h2 class="text-4xl mb-2 font-semibold">Informácie o produkte</h2>
                         <p>
-                            lorem, laoreet volutpat elit. Sed scelerisque nibh
-                            nisl, in bibendum nibh maximus tempor. Cras volutpat
-                            volutpat nisi, vel egestas neque pharetra id.
-                            Curabitur eget luctus justo, eget ullamcorper
-                            turpis. Duis at dolor vel turpis accumsan vulputate.
-                            Maecenas iaculis sit amet urna ut ullamcorper.
-                            Quisque a placerat eros. Morbi sed rutrum dolor. Sed
-                            eleifend erat lectus, ultrices tempus arcu laoreet
-                            in. Sed ac magna a felis varius euismod quis ut
-                            libero. Morbi ac ex at metus ultrices tincidunt.
-                            Curabitur cursus, eros eu tincidunt dignissim,
-                            lectus ante tempor lectus, nec faucibus nulla sapien
-                            a odio. Vestibulum a varius nulla, eu interdum nunc.
-                            Sed scelerisque mi at blandit consequat. Proin
-                            gravida augue nec augue faucibus pulvinar. Quisque
-                            tincidunt dapibus congue. Orci varius natoque
-                            penatibus et magnis dis parturient montes, nascetur
-                            ridiculus mus. Nunc sit amet iaculis nibh. Phasellus
-                            non pretium felis, in maximus est. Vestibulum
-                            interdum eget purus ut porta. Curabitur tortor elit,
-                            luctus vel justo eu, pellentesque congue ligula.
-                            Nulla facilisi. Nulla facilisi. Nulla facilisi.
-                            Nulla facilisi.
+                            {{ $product->description }}
                         </p>
                     </div>
                     <div class="d-flex flex-column">
                         <h2 class="text-4xl mb-2 font-semibold">Galéria</h2>
                         <div class="row row-cols-md-3 row-cols-2 g-2 ">
-                            @for ($i = 0; $i < 3; $i++)
-                                <img src="https://picsum.photos/200" alt="Product Image" class="img-fluid col" />
-                            @endfor
+                            @foreach ($product->gallery as $image)
+                                <div class="col">
+                                    <img src="{{ $image }}" alt="gallery image" class="w-100" />
+                                </div>
+                            @endforeach
                         </div>
                     </div>
                     <div class="d-flex flex-column">
@@ -98,10 +97,9 @@
                             <div class="d-flex flex-column">
                                 <h2 class="text-3xl mb-2 font-semibold">Ďalšie informácie</h2>
                                 <p>
-                                    <strong>Kategória produktu:</strong>
-                                    Category XYZ
+                                    <strong>Kategória produktu:</strong>{{ $product->category }}
                                 </p>
-                                <p><strong>Výrobca:</strong> XYZ</p>
+                                <p><strong>Výrobca:</strong> {{ $product->manufacturer }}</p>
                                 <!-- Add more details as required -->
                             </div>
                         </div>
